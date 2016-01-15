ef21dba140720c9e08b1090e2c6c28a3096df6a8
Dmitry Demyankov <dmitry.demyankov@gmail.com>
2016-01-15 15:27:21 +0100

    moving defaultConfig inside function

--- src/svgo.js
+++ src/svgo.js
@@ -1,14 +1,14 @@
 import Svgo from 'svgo';
 
-const defaultConfig = {
-  plugins: [
-    { removeTitle: true },
-    { removeDimensions: true },
-    { convertColors: { shorthex: true } },
-  ],
-};
-
 export default function (data, config = {}, callback) {
+  const defaultConfig = {
+    plugins: [
+      { removeTitle: true },
+      { removeDimensions: true },
+      { convertColors: { shorthex: true } },
+    ],
+  };
+
   const svgo = new Svgo(Object.assign({}, defaultConfig, config));
   svgo.optimize(data, result => callback(result));
 }
