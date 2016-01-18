8f5016b85cc10c718884f91b65048fec2dd5890a
Gustaf Zetterlund <gustaf.zetterlund@gmail.com>
2016-01-18 18:18:29 +0100

    fixed async issue

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import { toTemplateFunction } from './toTemplate';
 
 module.exports = function svgIconLoader(content) {
   this.cacheable(); // Flag loader as cacheable
+  const callback = this.async();
   const query = loaderUtils.parseQuery(this.query);
   const svgoEnabled = query.svgo !== undefined ? query.svgo : true;
 
-  svgo(content, query.svgoConfig,
-    ({ data: svg }) => svgoEnabled ? toTemplateFunction(svg) : toTemplateFunction(content));
+  svgo(content, query.svgoConfig, ({ data: svg }) => {
+    if (svgoEnabled) {
+      callback(null, toTemplateFunction(svg));
+    } else {
+      callback(null, toTemplateFunction(content));
+    }
+  });
 };
